07f83d5dc8c3ece649820b586ff25bff44be5c7c
rajrana22 <rajrana22@uchicago.edu>
2022-06-13 20:34:42 +0530

    RajRana: Added graph.py and made other small changes

--- graph.py
+++ graph.py
@@ -6,19 +6,21 @@ import matplotlib.pylab as plt
 from matplotlib.colors import LinearSegmentedColormap
 import re
 
-max_n = 17
-max_k = 3
+# net = network.
+# loc = local.
+# n = data shards.
+# k = parity shards.
+max_net_n = 2
+max_net_k = 1
+max_loc_n = 4
+max_loc_k = 2
 chunksize = 128
 mode = "i"
 throughput_filename = "throughput.log"
 heatmap_filename = "heatmap.png"
 
-def run_benchmark(n, k):
-    os.system(f"../run_benchmark.sh -n {n} -k {k} -c {chunksize} -m {mode} -f {throughput_filename}")
-
-def create_array():
-    array = [[np.nan for n in range(max_n + 1)] for k in range(max_k + 1)]
-    return array
+def run_benchmark(net_n, net_k, loc_n, loc_k):
+    os.system(f"../run_benchmark.sh -net_n {net_n} -net_k {net_k} -loc_n {loc_n} -loc_k {loc_k} -c {chunksize} -m {mode} -f {throughput_filename}")
 
 def generate_single_point(n, k, data):
     with open(throughput_filename) as f:
@@ -46,44 +48,47 @@ def generate_data(data):
             generate_single_point(n, k, data)
             os.remove(throughput_filename)
 
-def generate_heatmap(data):
-    array = np.array(data, dtype=float)
-    myColors = ("Black", "Purple", "Blue", "Red", "Pink", "Orange", "Yellow", "Green")
-    cmap = LinearSegmentedColormap.from_list("Custom", myColors, len(myColors))
-    mask = np.isnan(array)
-    ax = sns.heatmap(array, cmap=cmap, mask=mask, linewidths=0.5, cbar_kws={"label": "Throughput (MB/s)"})
-
-    # X-Y axis labels
-    ax.set_ylabel("Parity Units K")
-    ax.set_xlabel("Data Units N")
-    ax.invert_yaxis()
-
+def generate_scatterplot(data):
+    ...
 
 def parse_args():
-    global max_n
-    global max_k
+    max_net_n = args.nd
+    max_net_k = args.np
+    max_loc_n = args.ld
+    max_loc_k = args.lp
+    throughput_filename = args.b + ".log"
+    plot_filename = args.o + ".png"
+
+def parse_args():
+    global max_net_n
+    global max_net_k
+    global max_loc_n
+    global max_loc_k
     global chunksize
     global mode
     global heatmap_filename
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", help="Number of data shards", default=max_n, type=int)
-    parser.add_argument("-k", help="Number of parity shards", default=max_k, type=int)
+    parser.add_argument("-net_n", help="Number of network data shards", default=max_net_n, type=int)
+    parser.add_argument("-net_k", help="Number of network parity shards", default=max_net_k, type=int)
+    parser.add_argument("-loc_n", help="Number of local data shards", default=max_loc_n, type=int)
+    parser.add_argument("-loc_k", help="Number of local parity shards", default=max_loc_k, type=int)
     parser.add_argument("-c", help="Chunksize in KB", default=chunksize, type=int)
     parser.add_argument("-m", help="EC mode (i for ISA-L, j for JavaReedSolomon)", default=mode, type=str)
     parser.add_argument("-o", help="Output filename for heatmap", default=heatmap_filename, type=str)
     args = parser.parse_args()
-    max_n = args.n
-    max_k = args.k
+    max_net_n = args.net_n
+    max_net_k = args.net_k
+    max_loc_n = args.loc_n
+    max_loc_k = args.loc_k
     chunksize = args.c
     mode = args.m
     heatmap_filename = args.o
 
 def main():
     parse_args()
-    data = create_array()
-    generate_data(data)
+    data = generate_data()
     os.system("cd ..")
-    generate_heatmap(data)
+    generate_scatterplot(data)
     plt.savefig("../" + heatmap_filename)
     plt.show()
 
